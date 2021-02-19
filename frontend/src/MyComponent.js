44d5cd14e42643bef63c3c8ad7ad5b3dd1c9a58a
teekaygithub <tomoyuki.kato@yahoo.com>
2021-02-18 19:37:12 -0800

    Removed unnecessary components from the frontend. Created custom component. Removed obsolete styling from App.css

---
+++ frontend/src/MyComponent.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class MyComponent extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>My Component</h2>
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
